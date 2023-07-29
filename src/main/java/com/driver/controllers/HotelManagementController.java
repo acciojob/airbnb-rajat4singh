819537735929e6e6ffa48e42b24a3bae5cee43d3
Rajat <rs.sunny100@gmail.com>
2023-07-29 20:26:31 +0530

    code done

--- src/main/java/com/driver/controllers/HotelManagementController.java
+++ src/main/java/com/driver/controllers/HotelManagementController.java
@@ -12,14 +12,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Objects;
-import java.util.UUID;
+import java.util.*;
 
 @RestController
 @RequestMapping("/hotel")
 public class HotelManagementController {
+
+    HotelManagementService hotelManagementService = new HotelManagementService();
 
     @PostMapping("/add-hotel")
     public String addHotel(@RequestBody Hotel hotel){
@@ -29,8 +28,13 @@ public class HotelManagementController {
         //Incase somebody is trying to add the duplicate hotelName return FAILURE
         //in all other cases return SUCCESS after successfully adding the hotel to the hotelDb.
 
+        Optional<Boolean> hotelAdded = hotelManagementService.addHotel(hotel);
 
-        return null;
+        if(hotelAdded.isEmpty()){
+            return "FAILURE";
+        }
+
+        return "SUCCESS";
     }
 
     @PostMapping("/add-user")
@@ -39,7 +43,9 @@ public class HotelManagementController {
         //You need to add a User Object to the database
         //Assume that user will always be a valid user and return the aadharCardNo of the user
 
-       return null;
+        Optional<Integer> addedUser = hotelManagementService.addUser(user);
+
+        return addedUser.get();
     }
 
     @GetMapping("/get-hotel-with-most-facilities")
@@ -49,7 +55,9 @@ public class HotelManagementController {
         //Incase there is a tie return the lexicographically smaller hotelName
         //Incase there is not even a single hotel with atleast 1 facility return "" (empty string)
 
-        return null;
+        Optional<String> optionalHotel = hotelManagementService.getHotelWithMostFacilities();
+        if(optionalHotel.isEmpty()) return "";
+        return optionalHotel.get();
     }
 
     @PostMapping("/book-a-room")
@@ -59,17 +67,23 @@ public class HotelManagementController {
         //Have bookingId as a random UUID generated String
         //save the booking Entity and keep the bookingId as a primary key
         //Calculate the total amount paid by the person based on no. of rooms booked and price of the room per night.
-        //If there arent enough rooms available in the hotel that we are trying to book return -1 
-        //in other case return total amount paid 
-        
-        return 0;
+        //If there arent enough rooms available in the hotel that we are trying to book return -1
+        //in other case return total amount paid
+
+        Optional<Integer> optionalBooking = hotelManagementService.bookARoom(booking);
+
+        if(optionalBooking.isEmpty()) return -1;
+        return optionalBooking.get();
     }
-    
+
     @GetMapping("/get-bookings-by-a-person/{aadharCard}")
     public int getBookings(@PathVariable("aadharCard")Integer aadharCard)
     {
-        //In this function return the bookings done by a person 
-        return 0;
+        //In this function return the bookings done by a person
+
+        Optional<Integer> optionalBookings = hotelManagementService.getBookings(aadharCard);
+
+        return optionalBookings.get();
     }
 
     @PutMapping("/update-facilities")
@@ -79,7 +93,11 @@ public class HotelManagementController {
         //If the hotel is already having that facility ignore that facility otherwise add that facility in the hotelDb
         //return the final updated List of facilities and also update that in your hotelDb
         //Note that newFacilities can also have duplicate facilities possible
-        return null;
+
+        Optional<Hotel> optionalFacilities = hotelManagementService.updateFacilities(newFacilities,hotelName);
+
+        if(optionalFacilities.isEmpty()) return null;
+        return optionalFacilities.get();
     }
 
 }
